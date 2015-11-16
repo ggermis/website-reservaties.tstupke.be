11f2e9ce17942a4e31942cd46cbae10b4234631b
Gerrit Germis <gerrit.germis@up-nxt.com>
2015-11-16 07:32:24 +0100

    test

--- create_database_backup.sh
+++ create_database_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export MYSQL_PWD="mat82260"
+export MYSQL_PWD="${1:?No password specified}"
 /usr/bin/mysqldump --skip-add-locks --user=paulym_tstupke paulym_tstupke > /home/paulym/backups/paulym_tstupke-$(date +"%Y%m%d").sql
