a1af16fca444207d3bbc86192504d3eda248469a
Gerrit Germis <gerrit.germis@up-nxt.com>
2016-01-28 13:55:21 +0100

    rename email scripts

--- process_incoming_emails.sh
+++ process_incoming_emails.sh
@@ -3,4 +3,4 @@
 ROOT_DIR="${1:?No root directory specified}"
 PHP=$(which php)
 
-"$PHP" "${ROOT_DIR}/frontend/api/incoming_emails.php" &> "${ROOT_DIR}/frontend/logs.txt"
+"$PHP" "${ROOT_DIR}/frontend/api/incoming_emails.php"
--- process_outgoing_emails.sh
+++ process_outgoing_emails.sh
@@ -3,19 +3,18 @@
 ROOT_DIR="${1:?No root directory specified}"
 PHP="$(which php)"
 
-{
 # ----
 #  weekend
 # ----
 
 # 16 days after reservation, if still pending, send reminder that they have not paid yet
-"$PHP" "${ROOT_DIR}/frontend/api/scheduled_emails.php" reminder 15 pending weekend
+"$PHP" "${ROOT_DIR}/backend/scripts/outgoing_emails.php" reminder 15 pending weekend
 
 # 2 months before arriving, send instruction email
-"$PHP" "${ROOT_DIR}/frontend/api/scheduled_emails.php" instruction 60 confirmed weekend
+"$PHP" "${ROOT_DIR}/backend/scripts/outgoing_emails.php" instruction 60 confirmed weekend
 
 # 1 week before arriving, send arrival email
-"$PHP" "${ROOT_DIR}/frontend/api/scheduled_emails.php" arrival 7 confirmed weekend
+"$PHP" "${ROOT_DIR}/backend/scripts/outgoing_emails.php" arrival 7 confirmed weekend
 
 
 # ----
@@ -23,9 +22,7 @@ PHP="$(which php)"
 # ----
 
 # 16 days after reservation, if still pending, send reminder that they have not paid yet
-"$PHP" "${ROOT_DIR}/frontend/api/scheduled_emails.php" reminder 15 pending bivak
+"$PHP" "${ROOT_DIR}/backend/scripts/outgoing_emails.php" reminder 15 pending bivak
 
 # 2 months before arriving, send instruction email
-"$PHP" "${ROOT_DIR}/frontend/api/scheduled_emails.php" instruction 60 confirmed bivak
-
-} &> "${ROOT_DIR}/frontend/logs.txt"
+"$PHP" "${ROOT_DIR}/backend/scripts/outgoing_emails.php" instruction 60 confirmed bivak
